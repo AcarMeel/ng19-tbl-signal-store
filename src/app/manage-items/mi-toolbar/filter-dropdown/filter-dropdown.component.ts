9226b569d139019f4ffd683c93f7217044cf6e45
Melissa <acarmeel@gmail.com>
2025-04-19 17:32:01 -0600

    Refactor dropdown

--- src/app/manage-items/mi-toolbar/filter-dropdown/filter-dropdown.component.ts
+++ src/app/manage-items/mi-toolbar/filter-dropdown/filter-dropdown.component.ts
@@ -1,10 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, input, OnInit, output, signal } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { ISelector } from '../../../core/interfaces/selector.interface';
-import { MIDataStore } from '../../store/data.store';
 
 @Component({
   selector: 'app-filter-dropdown',
@@ -15,16 +14,22 @@ import { MIDataStore } from '../../store/data.store';
   templateUrl: './filter-dropdown.component.html',
   styleUrl: './filter-dropdown.component.scss'
 })
-export class FilterDropdownComponent {
-  readonly miStore = inject(MIDataStore);
+export class FilterDropdownComponent implements OnInit {
+  defaultFilterSelected = input<string>('');
+  totalResults = input<number>(0);
   filters = signal<ISelector[]>([
     { value: 'NONE', viewValue: 'Select an option'},
     { value: 'ALL', viewValue: 'All'},
     { value: 'FAV', viewValue: 'Favorite'}
   ]);
-  filter = new FormControl(this.miStore.filter());
+  filterControl = new FormControl<string>('');
+  filterChange = output<string>();
+
+  ngOnInit(): void {
+    this.filterControl.setValue(this.defaultFilterSelected());
+  }
 
   public onFilterSelected(): void {
-    this.miStore.setFilter(this.filter.value || 'NONE');
+    this.filterChange.emit(this.filterControl.value || '');
   }
 }
--- src/app/manage-items/mi-toolbar/mi-toolbar.component.ts
+++ src/app/manage-items/mi-toolbar/mi-toolbar.component.ts
@@ -1,18 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { DownloadReportComponent } from './download-report/download-report.component';
 import { FilterDropdownComponent } from './filter-dropdown/filter-dropdown.component';
 import { PageSizeComponent } from './page-size/page-size.component';
+import { MIDataStore } from '../store/data.store';
 
 @Component({
-  selector: 'app-mi-toolbar',
-  imports: [
-    DownloadReportComponent,
-    FilterDropdownComponent,
-    PageSizeComponent
-  ],
-  templateUrl: './mi-toolbar.component.html',
-  styleUrl: './mi-toolbar.component.scss'
+    selector: 'app-mi-toolbar',
+    imports: [
+        DownloadReportComponent,
+        FilterDropdownComponent,
+        PageSizeComponent
+    ],
+    templateUrl: './mi-toolbar.component.html',
+    styleUrl: './mi-toolbar.component.scss'
 })
 export class MiToolbarComponent {
+    readonly miStore = inject(MIDataStore);
+
+    public setFilterSelected(filter: string): void {
+        this.miStore.setFilter(filter || 'NONE');
+    }
 
 }
